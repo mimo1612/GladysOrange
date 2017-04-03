bfb8b18fb86ca7e9b9ac068cad2ca024a968f6c3
mimo1612 <sebmalizia@gmail.com>
2017-04-03 21:51:24 +0200

    Erreur install

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 module.exports = function(sails) {
     
     var execComamand = require('./lib/OrangeCommande.js');
-	var install = require('./lib/install.js');
+	//var install = require('./lib/install.js');
 
     return {
-        execCommand : execCommand,
-		install : install
+        execCommand : execCommand//,
+		//install : install
     };
 };
