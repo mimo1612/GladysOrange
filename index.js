56f6d3e1a4c90d277b4682f05871bb77af06e522
mimo1612 <sebmalizia@gmail.com>
2017-04-04 19:12:39 +0200

    Réinitialisation du projet

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-
-
 module.exports = function(sails) {
-    
-    var execComamand = require('./lib/OrangeCommande.js');
-	//var install = require('./lib/install.js');
+
+    var getWeather = require('./lib/getWeather.js');
 
     return {
-        execCommand : execCommand//,
-		//install : install
+        getWeather: getWeather
     };
 };
+
+/*
+module.exports = function(sails) {
+    
+    var comm = require('./lib/OrangeCommande.js');
+	var install = require('./lib/install.js');
+
+    return {
+        comm : comm,
+		install : install
+    };
+};*/
